dd17e8c83560c7ca3ddefb7596b3789c63faf077
bharadwajvyadavalli <educationyvb@pm.me>
2023-03-05 01:03:51 -0600

    Commit code

--- 25. Custom Data Structures/Kth Smallest Number (hard).py
+++ 25. Custom Data Structures/Kth Smallest Number (hard).py
@@ -1,42 +1,4 @@
-'''
-Problem Statement 
-Given an unsorted array of numbers, find Kth smallest number in it.
 
-Please note that it is the Kth smallest number in the sorted order, not the Kth distinct element.
-
-Example 1:
-
-Input: [1, 5, 12, 2, 11, 5], K = 3
-Output: 5
-Explanation: The 3rd smallest number is '5', as the first two smaller numbers are [1, 2].
-Example 2:
-
-Input: [1, 5, 12, 2, 11, 5], K = 4
-Output: 5
-Explanation: The 4th smallest number is '5', as the first three smaller numbers are
-[1, 2, 5].
-Example 3:
-
-Input: [5, 12, 11, -1, 12], K = 3
-Output: 11
-Explanation: The 3rd smallest number is '11', as the first two small numbers are [5, -1].
-'''
-
-
-#mycode
-from heapq import *
-
-def find_Kth_smallest_number(nums, k):
-  # TODO: Write your code here
-  heap = []
-  for num in nums:
-    if len(heap) < k:
-      heappush(heap, -num)
-    else:
-      if -num > heap[0]:
-        heappop(heap)
-        heappush(heap, -num)
-  return -heap[0]
 
 
 def main():
@@ -192,38 +154,7 @@ Overall, the time complexity of this algorithm will be O(N*logN+K*logN) and the 
 '''
 
 
-#5. Using Partition Scheme of Quicksort 
-def find_Kth_smallest_number(nums, k):
-  return find_Kth_smallest_number_rec(nums, k, 0, len(nums) - 1)
 
-
-def find_Kth_smallest_number_rec(nums, k, start, end):
-  p = partition(nums, start, end)
-
-  if p == k - 1:
-    return nums[p]
-
-  if p > k - 1:  # search lower part
-    return find_Kth_smallest_number_rec(nums, k, start, p - 1)
-
-  # search higher part
-  return find_Kth_smallest_number_rec(nums, k, p + 1, end)
-
-
-def partition(nums, low, high):
-  if low == high:
-    return low
-
-  pivot = nums[high]
-  for i in range(low, high):
-    # all elements less than 'pivot' will be before the index 'low'
-    if nums[i] < pivot:
-      nums[low], nums[i] = nums[i], nums[low]
-      low += 1
-
-  # put the pivot in its correct place
-  nums[low], nums[high] = nums[high], nums[low]
-  return low
 
 
 def main():
--- 25. Custom Data Structures/random_set.py
+++ 25. Custom Data Structures/random_set.py
@@ -1,49 +1,4 @@
-from random import choice
 
-
-class RandomSet():
-    def __init__(self):
-        self.indexor = {}  # Maps the actual value to its index
-        self.store = []   # Store the actual values in an array
-
-    # Function to insert a value
-    def insert(self, val):
-        """
-        Inserts a value in the data structure.
-        Returns True if it did not already contain the specified element.
-        """
-        if val in self.indexor:
-            return False
-        # Insert the actual value as a key and its index as a value
-        self.indexor[val] = len(self.store)
-        # Append a new value to array
-        self.store.append(val)
-        return True
-
-    # Function to remove a value
-    def delete(self, val):
-        """
-        Removes a value from the data structure.
-        Returns True if it contains the specified element.
-        """
-        if val in self.indexor:
-            # swap the last element in the array with the element
-            # to delete, update the location of the moved element
-            # in its entry in the hash map
-            last, i = self.store[-1], self.indexor[val]
-            self.store[i], self.indexor[last] = last, i
-            # delete the last element
-            del self.indexor[val]
-            self.store.pop()
-            return True
-        return False
-
-    # Function to generate a random value
-    def get_random(self):
-        """
-        Choose an element at random from the data structure.
-        """
-        return choice(self.store)
 
 
 # Driver code
