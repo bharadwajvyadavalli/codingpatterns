442182b801387ac5bfa55aad0893f16e23eb56a3
bharadwajvyadavalli <educationyvb@pm.me>
2023-03-04 21:44:34 -0600

    created bit-wise xor notebook

--- 12. Bitwise XOR/Two Single Numbers (medium).py
+++ 12. Bitwise XOR/Two Single Numbers (medium).py
@@ -1,38 +1,9 @@
 '''
-Problem Statement 
-In a non-empty array of numbers, every number appears exactly twice except two numbers that appear only once. Find the two numbers that appear only once.
 
-Example 1:
-
-Input: [1, 4, 2, 1, 3, 5, 6, 2, 3, 5]
-Output: [4, 6]
-Example 2:
-
-Input: [2, 1, 3, 2]
-Output: [1, 3]
 '''
 
 #mycode
-def find_single_numbers(nums):
-  # TODO: Write your code here
-  sum = 0
-  for num in nums:
-    sum ^= num
-  
-  diff = 1
-  while (diff & sum == 0):
-    diff = diff << 1
-  
-  num1, num2 = 0, 0
 
-  for num in nums:
-    if num & diff ==0:
-      num1 ^= num
-    else:
-      num2 ^= num
-
-
-  return [num1, num2]
 
 
 def main():
